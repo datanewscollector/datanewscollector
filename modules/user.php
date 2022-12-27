6b7d4552a0902a8a7f6cd0722cae66bbe97bfabc
busticketing <johnnynoun5@gmail.com>
2022-12-27 22:06:35 +0200

    creating folder modules

---
+++ modules/user.php
@@ -0,0 +1,18 @@
+<?php
+
+
+	// Create a connection
+	$conn = mysqli_connect("localhost", "root", "", "DNC");
+
+	if($conn) {
+		echo "success";
+	}
+	else {
+		die("Error". mysqli_connect_error());
+	}
+?>
+<?php
+user{
+
+}
+?>
