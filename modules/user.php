32d5cd17350515471eb1d8cacb2893a56da395bd
busticketing <johnnynoun5@gmail.com>
2022-12-27 23:04:01 +0200

    php

--- modules/user.php
+++ modules/user.php
@@ -9,8 +9,9 @@ else {
 	die("Error". mysqli_connect_error());
 }
 user{
-
+	
 	if(isset($_POST['m_login_signup_submit'])){
+
 		$username=$_POST["UserName"];
 		$firstname=$_POST["FirstName"];
 		$lastname=$_POST["LastName"];
