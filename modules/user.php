0756e9366004bcbd2660405c6fe32bc878fb78c8
busticketing <johnnynoun5@gmail.com>
2022-12-27 22:24:55 +0200

    signup php adding

--- modules/user.php
+++ modules/user.php
@@ -1,5 +1,5 @@
 <?php
-
+user{
 
 	// Create a connection
 	$conn = mysqli_connect("localhost", "root", "", "DNC");
@@ -10,9 +10,18 @@
 	else {
 		die("Error". mysqli_connect_error());
 	}
-?>
-<?php
-user{
-
+	if($_SERVER["REQUEST_METHOD"] == "POST"){
+		$username=$_POST["UserName"];
+		$firstname=$_POST["FirstName"];
+		$lastname=$_POST["LastName"];
+		$email=$_POST["email"];
+		$password=$_POST["password"];
+	}
+	
+    $sql = "Select * from users where username='$username'";
+    
+    $result = mysqli_query($conn, $sql);
+    
+    $num = mysqli_num_rows($result);
 }
 ?>
